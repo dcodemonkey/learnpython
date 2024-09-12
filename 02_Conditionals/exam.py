c67710ebaf29f0c54eefd32e54aa419b7725c9c7
dcodemonkey <in.developer@outlook.com>
2024-09-12 18:48:56 +0530

    Adding new file exam.py asked Amit bhaiya

---
+++ 02_Conditionals/exam.py
@@ -0,0 +1,27 @@
+# print('Hello, Amit Ji')
+
+# def kunal(a,b):
+#   # print(a,b)
+#   return a,b
+
+# x = kunal(3,5)
+# print(x)
+
+# x = (1,2,3)
+# x[0]=4
+# print(x)
+
+print([x*x for x in range(14,-1,-2)])
+
+d = {True:"a"}
+print("========",d.get("s",0))
+
+# print(d["s"])
+d.update({"1":23})
+print("___________________",d)
+for e in d.keys():
+  print(e)
+
+print(d.keys())
+print(d.items())
+print("testing git")
